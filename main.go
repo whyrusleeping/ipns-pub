affba5a4bf8f3e96edefcee6087d1b2d9f9e8354
Jeromy <jeromyj@gmail.com>
2016-02-08 10:35:07 -0800

    add option to start a daemon to publish on a given interval

--- main.go
+++ main.go
@@ -28,32 +28,6 @@ import (
 
 const IpnsValidatorTag = "ipns"
 
-func getBootstrapAddrs() ([]ma.Multiaddr, error) {
-	addrs := []string{
-		"/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-		"/ip4/104.236.151.122/tcp/4001/ipfs/QmSoLju6m7xTh3DuokvT3886QRYqxAzb1kShaanJgW36yx",
-		"/ip4/104.236.176.52/tcp/4001/ipfs/QmSoLnSGccFuZQJzRadHn95W2CrSFmZuTdDWP8HXaHca9z",
-		"/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
-		"/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
-		"/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
-		"/ip4/162.243.248.213/tcp/4001/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm",
-		"/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
-		"/ip4/178.62.61.185/tcp/4001/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3",
-	}
-
-	var maddrs []ma.Multiaddr
-	for _, s := range addrs {
-		a, err := ma.NewMultiaddr(s)
-		if err != nil {
-			return nil, err
-		}
-
-		maddrs = append(maddrs, a)
-	}
-
-	return maddrs, nil
-}
-
 func fatal(i interface{}) {
 	fmt.Fprintln(os.Stderr, i)
 	os.Exit(1)
@@ -83,6 +57,14 @@ And then run:
 			Name:  "key",
 			Usage: "specify file containing key to publish with",
 		},
+		cli.BoolFlag{
+			Name:  "daemon",
+			Usage: "run a daemon that republishes the given ipns entry",
+		},
+		cli.StringFlag{
+			Name:  "interval",
+			Usage: "specify the republish interval for daemon mode (default 12h)",
+		},
 	}
 	app.Action = pubFunc
 
@@ -90,15 +72,6 @@ And then run:
 	if err != nil {
 		fatal(err)
 	}
-}
-
-func loadKeyFile(fi string) (ci.PrivKey, error) {
-	data, err := ioutil.ReadFile(fi)
-	if err != nil {
-		return nil, err
-	}
-
-	return ci.UnmarshalPrivateKey(data)
 }
 
 func pubFunc(c *cli.Context) {
@@ -123,6 +96,17 @@ func pubFunc(c *cli.Context) {
 		fatal(err)
 	}
 
+	interv := c.String("interval")
+	var ticktime time.Duration = time.Hour * 12
+	if interv != "" {
+		d, err := time.ParseDuration(interv)
+		if err != nil {
+			fatal(err)
+		}
+
+		ticktime = d
+	}
+
 	bs, err := getBootstrapAddrs()
 	if err != nil {
 		fatal(err)
@@ -135,10 +119,65 @@ func pubFunc(c *cli.Context) {
 		fatal(err)
 	}
 
-	err = pubPath(dht, priv, dstore, p)
+	nsys := namesys.NewRoutingPublisher(dht, dstore)
+
+	// publish once in either case
+	err = nsys.Publish(context.TODO(), priv, p)
 	if err != nil {
 		fatal(err)
 	}
+
+	if !c.Bool("daemon") {
+		return
+	}
+
+	for range time.Tick(ticktime) {
+		fmt.Println("publishing...")
+		before := time.Now()
+		err = nsys.Publish(context.TODO(), priv, p)
+		if err != nil {
+			// TODO: probably don't want to actually error out and die here
+			fatal(err)
+		}
+		fmt.Println("publish took: ", time.Now().Sub(before))
+	}
+}
+
+// currently just uses the ipfs core bootstrap nodes
+// TODO: allow user to specify their own
+func getBootstrapAddrs() ([]ma.Multiaddr, error) {
+	addrs := []string{
+		"/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+		"/ip4/104.236.151.122/tcp/4001/ipfs/QmSoLju6m7xTh3DuokvT3886QRYqxAzb1kShaanJgW36yx",
+		"/ip4/104.236.176.52/tcp/4001/ipfs/QmSoLnSGccFuZQJzRadHn95W2CrSFmZuTdDWP8HXaHca9z",
+		"/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
+		"/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+		"/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
+		"/ip4/162.243.248.213/tcp/4001/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm",
+		"/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
+		"/ip4/178.62.61.185/tcp/4001/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3",
+	}
+
+	var maddrs []ma.Multiaddr
+	for _, s := range addrs {
+		a, err := ma.NewMultiaddr(s)
+		if err != nil {
+			return nil, err
+		}
+
+		maddrs = append(maddrs, a)
+	}
+
+	return maddrs, nil
+}
+
+func loadKeyFile(fi string) (ci.PrivKey, error) {
+	data, err := ioutil.ReadFile(fi)
+	if err != nil {
+		return nil, err
+	}
+
+	return ci.UnmarshalPrivateKey(data)
 }
 
 func spawnDHT(pk ci.PrivKey, dstore repo.Datastore, bootstraps []ma.Multiaddr) (*dht.IpfsDHT, error) {
@@ -154,14 +193,6 @@ func spawnDHT(pk ci.PrivKey, dstore repo.Datastore, bootstraps []ma.Multiaddr) (
 	ps := peer.NewPeerstore()
 	ps.AddPrivKey(local, pk)
 	ps.AddPubKey(local, pub)
-
-	if len(bootstraps) > 0 {
-		fmt.Println("Bootstrapping to:")
-		for _, b := range bootstraps {
-			fmt.Printf("  - %s\n", b)
-		}
-		fmt.Println()
-	}
 
 	listenaddr, err := ma.NewMultiaddr("/ip4/0.0.0.0/tcp/0")
 	if err != nil {
@@ -179,6 +210,23 @@ func spawnDHT(pk ci.PrivKey, dstore repo.Datastore, bootstraps []ma.Multiaddr) (
 	idht.Validator[IpnsValidatorTag] = namesys.IpnsRecordValidator
 	idht.Selector[IpnsValidatorTag] = namesys.IpnsSelectorFunc
 
+	err = doBootstrap(host, idht, bootstraps)
+	if err != nil {
+		return nil, err
+	}
+
+	return idht, nil
+}
+
+func doBootstrap(bh *basichost.BasicHost, idht *dht.IpfsDHT, bootstraps []ma.Multiaddr) error {
+	if len(bootstraps) > 0 {
+		fmt.Println("Bootstrapping to:")
+		for _, b := range bootstraps {
+			fmt.Printf("  - %s\n", b)
+		}
+		fmt.Println()
+	}
+
 	errs := make(chan error)
 	for _, bsaddr := range bootstraps {
 		go func(bsa ma.Multiaddr) {
@@ -189,15 +237,19 @@ func spawnDHT(pk ci.PrivKey, dstore repo.Datastore, bootstraps []ma.Multiaddr) (
 				return
 			}
 
-			ps.AddAddr(iaddr.ID(), iaddr.Transport(), peer.PermanentAddrTTL)
+			bh.Peerstore().AddAddr(iaddr.ID(), iaddr.Transport(), peer.PermanentAddrTTL)
 
-			ctx, _ := context.WithTimeout(context.Background(), time.Second*10)
-			err = host.Connect(ctx, ps.PeerInfo(iaddr.ID()))
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+
+			pi := bh.Peerstore().PeerInfo(iaddr.ID())
+			err = bh.Connect(ctx, pi)
 			if err != nil {
 				fmt.Printf("error connecting to peer: %s\n", err)
 				errs <- err
 				return
 			}
+
 			fmt.Printf("dial to %s succeeded!\n", iaddr.ID())
 
 			idht.Update(context.TODO(), iaddr.ID())
@@ -213,14 +265,8 @@ func spawnDHT(pk ci.PrivKey, dstore repo.Datastore, bootstraps []ma.Multiaddr) (
 		}
 	}
 	if good == 0 {
-		fatal("couldnt connect to any bootstrap peers")
+		return fmt.Errorf("couldnt connect to any bootstrap peers")
 	}
 
-	return idht, nil
-}
-
-func pubPath(dht *dht.IpfsDHT, privk ci.PrivKey, dstore repo.Datastore, p path.Path) error {
-	pub := namesys.NewRoutingPublisher(dht, dstore)
-	ctx := context.Background()
-	return pub.Publish(ctx, privk, p)
+	return nil
 }
